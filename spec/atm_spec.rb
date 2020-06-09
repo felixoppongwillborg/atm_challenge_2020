1ce79e7f48ef89fd25840928b80e3dfa9e2ab717
Sima <sima.boreiri@gmail.com>
2020-06-09 12:08:05 +0200

    define instance_double to rspec file

--- spec/atm_spec.rb
+++ spec/atm_spec.rb
@@ -1,5 +1,10 @@
 require './lib/atm.rb'
 describe Atm do 
+    let (:account) { instance_double ('Account') }
+    before do 
+        allow(account).to receive(:balance).and_return(100)
+        allow(account).to receive(:balance=)
+    end
     it 'has 1000€ on initialize' do
         expect(subject.funds).to eq 1000
     end
