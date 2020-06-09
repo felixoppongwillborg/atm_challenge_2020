7f8ec41580977ceb63d98f701e03c9d802a19075
Sima <sima.boreiri@gmail.com>
2020-06-09 15:48:59 +0200

    adding instance_double (account), writting test and pass

--- lib/atm.rb
+++ lib/atm.rb
@@ -4,7 +4,14 @@ class Atm
     def initialize 
         @funds = 1000
     end
-    def withdraw (amount)
+    def withdraw (amount, account)
+        case
+        when amount > account.balance
+            return 
+        else 
         @funds -= amount
+        account.balance = account.balance - amount
+        { status: true, message: 'success', date: Date.today, amount: amount }
+        end
     end
 end
--- spec/atm_spec.rb
+++ spec/atm_spec.rb
@@ -1,15 +1,22 @@
 require './lib/atm.rb'
+require 'date'
 describe Atm do 
     let (:account) { instance_double ('Account') }
     before do 
         allow(account).to receive(:balance).and_return(100)
         allow(account).to receive(:balance=)
+    end 
+    it ' allow withdraw if the account has enough balance.' do 
+        expected_output = { status: true, message: 'success', date: Date.today, amount: 45 }
+        expect(subject.withdraw(45, account)).to eq expected_output
     end
+
     it 'has 1000€ on initialize' do
         expect(subject.funds).to eq 1000
     end
     it 'funds are reduced at withdraw' do 
-        subject.withdraw 50 
+        subject.withdraw(50, account)
         expect(subject.funds).to eq 950
     end 
+
 end 
