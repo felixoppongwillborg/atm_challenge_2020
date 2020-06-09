fd5ae239dbe5ea9b48e3809c2a0edf0cdb880de4
felixoppongwillborg <felixwillborg@MacBook-Pro.local>
2020-06-09 17:16:35 +0200

    added pincode and insufficient funds in ATM wrote both test and passed them both

--- lib/atm.rb
+++ lib/atm.rb
@@ -4,16 +4,29 @@ class Atm
     def initialize 
         @funds = 1000
     end
-    def withdraw (amount, account)
+    def withdraw (amount, pin_code, account)
         case
+        when incorrect_pin?(pin_code, account.pin_code)
+            { status: false, message: 'wrong pin', date: Date.today }
         when insufficient_funds_in_account?(amount, account)
             { status: false, message: 'insufficient funds', date: Date.today }
+        when insufficient_funds_in_atm?(amount)
+            { status: false, message: 'insufficient funds in ATM', date: Date.today }
         else 
             perform_transaction(amount, account)
         end
     end
 
     private
+    
+    def incorrect_pin?(pin_code, actual_pin)
+        pin_code != actual_pin
+    end
+
+    def insufficient_funds_in_atm?(amount)
+        @funds < amount
+    end
+
     def insufficient_funds_in_account?(amount, account)
         amount > account.balance
     end
