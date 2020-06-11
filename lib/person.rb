27a90158194b0bdf971178645018803807a0bc43
Sima <sima.boreiri@gmail.com>
2020-06-11 17:10:43 +0200

    write the test for raise error if name is missing and pass it

--- lib/person.rb
+++ lib/person.rb
@@ -1,6 +1,14 @@
 class Person 
     attr_accessor :name, :cash, :account
-    def initialize 
-        @name = :name
+    def initialize (attrs = {})
+       set_name(attrs[:name])
+
+    end
+    def set_name(obj)
+        obj == nil ? missing_name : @name = obj
+    end
+
+    def missing_name 
+        raise 'A name is required'
     end
 end
