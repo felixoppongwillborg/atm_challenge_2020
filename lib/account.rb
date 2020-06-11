67c37dd300d67835ea303e1e745432daa65bab2e
felixoppongwillborg <felixwillborg@MacBook-Pro.local>
2020-06-11 13:22:02 +0200

    wrote both test and code for exp_date and set exp date to be 5 yrs a head

--- lib/account.rb
+++ lib/account.rb
@@ -1,8 +1,16 @@
 class Account
     attr_accessor :exp_date, :pin_code, :balance, :account_status
+    
+    STANDARD_VALIDITY_YRS = 5
+
     def initialize 
-        @pin_code = rand(1000..9999)
-       
+        @pin_code = rand(1000..9999) 
+        @exp_date = set_expire_date
+        
+    end  
+   
+    def set_expire_date
+        Date.today.next_year(STANDARD_VALIDITY_YRS).strftime('%m/%y')
     end
 
    
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -6,6 +6,10 @@ require 'date'
         pin_code_length = Math.log10(subject.pin_code).to_i + 1
         expect(pin_code_length).to eq 4
     end   
+    it 'is expected to have an expiry date on initialize' do
+        expected_date = Date.today.next_year(5).strftime("%m/%y")
+        expect(subject.exp_date).to eq expected_date
+    end
 
 
 end
